e6f7434c3b068de1e51ff1551b5c6c107e6fd926
cdo <cdo@VPC01.vienxucorp.com>
2013-06-04 23:14:43 -0400

    New files for DeleteDirectory from AirBreather.

---
+++ ChinhDo.Transactions.FileManager/DeleteFileOperation.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace ChinhDo.Transactions
+{
+    /// <summary>
+    /// Rollbackable operation which deletes a file. An exception is not thrown if the file does not exist.
+    /// </summary>
+    sealed class DeleteFileOperation : SingleFileOperation
+    {
+        /// <summary>
+        /// Instantiates the class.
+        /// </summary>
+        /// <param name="path">The file to be deleted.</param>
+        public DeleteFileOperation(string path)
+            : base(path)
+        {
+        }
+
+        public override void Execute()
+        {
+            if (File.Exists(path))
+            {
+                string temp = FileUtils.GetTempFileName(Path.GetExtension(path));
+                File.Copy(path, temp);
+                backupPath = temp;
+            }
+
+            File.Delete(path);
+        }
+    }
+}
