416676e4fbc677640b1f84fc844ff971e0b12987
Chinh Do <cdo@vienxu.com>
2013-09-02 12:42:40 -0400

    New operation WriteAllBytes (By jrnail23) From Pull Request https://transactionalfilemgr.codeplex.com/SourceControl/network/forks/jrnail23/TransactionalFileManager/contribution/5311

---
+++ ChinhDo.Transactions.FileManager/WriteAllBytesOperation.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace ChinhDo.Transactions
+{
+    /// <summary>
+    /// Creates a file, and writes the specified contents to it.
+    /// </summary>
+    sealed class WriteAllBytesOperation : SingleFileOperation
+    {
+        private readonly byte[] contents;
+
+        /// <summary>
+        /// Instantiates the class.
+        /// </summary>
+        /// <param name="path">The file to write to.</param>
+        /// <param name="contents">The string to write to the file.</param>
+        public WriteAllBytesOperation(string path, byte[] contents)
+            : base(path)
+        {
+            this.contents = contents;
+        }
+
+        public override void Execute()
+        {
+            if (File.Exists(path))
+            {
+                string temp = FileUtils.GetTempFileName(Path.GetExtension(path));
+                File.Copy(path, temp);
+                backupPath = temp;
+            }
+
+            File.WriteAllBytes(path, contents);
+        }
+    }
+}
