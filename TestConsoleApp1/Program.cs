4ab9f6beb8e27ca8381392c3fa08b3496a6f333d
Chinh Do <cdo@vienxu.com>
2020-03-21 16:13:00 -0400

    Stress test

--- TestConsoleApp1/Program.cs
+++ TestConsoleApp1/Program.cs
@@ -10,20 +10,30 @@ namespace ChinhDo.Transactions.TestConsoleApp1
         {
             Console.WriteLine("Test .NET Core app");
 
+            RunStressTest();
+        }
+
+        static void RunStressTest()
+        {
+            // Start each test in its own thread and repeat for a few interations
+            const int iterations = 100000;
+            const string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
             IFileManager fm = new TxFileManager();
-
-            for (int i=0; i<25; i++)
+            string f1 = fm.GetTempFileName();
+            for (int i = 0; i < iterations; i++)
             {
-                using (TransactionScope scope1 = new TransactionScope())
+                using (TransactionScope s1 = new TransactionScope())
                 {
-                    String f1 = "TestFile1.txt";
-                    fm.AppendAllText(f1, "Hello");
-                    Console.WriteLine("Appended some text to " + f1 + ".");
-                    scope1.Complete();
+                    if (i % 100 == 0) {Console.WriteLine(i);}
+                    
+                    fm.AppendAllText(f1, text);
+                    s1.Complete();
                 }
-            }
 
-            Console.WriteLine(TxFileManager.GetEnlistmentCount());
+                Thread.Sleep(25);
+
+            }
         }
 
     }
